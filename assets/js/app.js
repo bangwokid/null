12df941a77b2730099d65a252003a250ac7c989e
bangwokid <78712874+bangwokid@users.noreply.github.com>
2026-07-27 10:07:41 +0700

    Add initial app.js with greeting messages

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+(() => {
+
+console.clear();
+
+const style =
+"color:#00d084;font-size:18px;font-weight:bold";
+
+console.log("%cHello World!", "font-size:40px;color:#0066ff;");
+console.log("Have a great day!");
+
+})();
